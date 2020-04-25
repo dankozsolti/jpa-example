34792a8b225ebf33aedddc534a20e23bb3e4842e
dankozsolti <dankozsolti@freemail.hu>
2020-04-25 17:37:45 +0200

    Added person package

---
+++ src/main/java/person/Main.java
@@ -0,0 +1,44 @@
+package person;
+
+
+import com.github.javafaker.Faker;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.ZoneId;
+import java.util.Locale;
+
+public class Main {
+
+    private static Faker faker = new Faker(new Locale("hu"));
+    public static Person randomPerson() {
+        Person person = Person.builder()
+                .name(faker.name().name())
+                .dob(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                .gender(faker.options().option(Person.Gender.class))
+                .address(Address.builder()
+                        .country(faker.address().country())
+                        .state(faker.address().state())
+                        .city(faker.address().city())
+                        .streetAddress(faker.address().streetAddress())
+                        .zip(faker.address().zipCode())
+                        .build())
+                .email(faker.internet().emailAddress())
+                .profession(faker.company().profession())
+                .build();
+        return person;
+    }
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-example");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        for(int i=0; i < 634; i++)
+            em.persist(randomPerson());
+        em.getTransaction().commit();
+        System.out.println(em.find(Person.class, Long.valueOf(42)));
+        em.close();
+        emf.close();
+    }
+}
